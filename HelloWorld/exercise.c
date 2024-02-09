a8c038439a4cc73bab30c81018ffc2ea0d3828cc
mahbubur3 <mahbubur9@yahoo.com>
2024-02-09 19:34:14 +0600

    added exercise file

---
+++ HelloWorld/exercise.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+// even and odd
+/*
+int main() {
+    int a;
+    printf("Enter a number: ");
+    scanf("%d", &a);
+
+    if (a % 2 == 0){
+        printf("%d is even", a);
+    } else {
+        printf("%d is odd", a);
+    }
+}
+*/
