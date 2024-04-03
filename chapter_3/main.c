a2c12f955ac6b608faad40e3b27bf08038b86112
mahbubur3 <mahbubur9@yahoo.com>
2024-04-03 19:35:03 +0600

    updated and added

---
+++ chapter_3/main.c
@@ -0,0 +1,239 @@
+#include <stdio.h>
+
+/*
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        printf("Hello\n");
+    }
+
+    return 0;
+}
+*/
+
+
+// int main() {
+//     for (int i = 1; i <= 10; i++) {
+//         printf("%d\n", i);
+//     }
+
+//     return 0;
+// }
+
+
+
+// int main() {
+//     for (int i = 2; i <= 10; i = i + 2) {
+//         printf("%d\n", i);
+//     }
+
+//     return 0;
+// }
+
+
+// int main() {
+//     for (int i = 20; i >= 1; i--) {
+//         printf("%d\n", i);
+//     }
+
+//     return 0;
+// }
+
+
+/*
+int main() {
+    int sum = 0, n;
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        sum = sum + i;
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
+*/
+
+
+/*
+int main() {
+    int sum = 0;
+
+    for(int i = 1; i <= 100; i++) {
+        sum = sum + i;
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
+*/
+
+
+/*
+int main() {
+    for (int i = 1; i <= 20; i++) {
+
+        if(i % 2 == 0) {
+            printf("%d - even\n", i);
+        } else {
+            printf("%d - odd\n", i);
+        }
+    }
+
+    return 0;
+}
+*/
+
+
+/*
+int main() {
+    for (int i = 1; i <= 10; i++) {
+
+        if(i == 5) {
+            break;
+        }
+
+        printf("%d", i);
+    }
+
+    return 0;
+}
+*/
+
+
+/*
+int main() {
+    for (int i = 1; i <= 10; i++) {
+
+        printf("%d", i);
+
+        if(i == 5) {
+            break;
+        }
+    }
+
+    return 0;
+}
+*/
+
+
+/*
+int main() {
+    for (int i = 1; i <= 10; i++) {
+
+        if(i == 5) {
+            continue;
+        }
+
+        printf("%d", i);
+    }
+
+    return 0;
+}
+*/
+
+
+// int main() {
+//     int i = 1;
+
+//     while (i <= 10) {
+//         printf("%d\n", i);
+//         i++;
+//     }
+
+//     return 0;
+// }
+
+
+/*
+int main() {
+    int i = 1;
+
+    while (i <= 10) {
+        if (i == 6) {
+            break;
+        }
+
+        printf("%d", i);
+        i++;
+    }
+
+    return 0;
+}
+*/
+
+
+/*
+int main() {
+    int i = 1;
+
+    do {
+        printf("%d\n", i);
+        i++;
+    }
+    while (i <= 5);
+
+    return 0;
+}
+*/
+
+
+
+// QUIZ \\\\\\\\\\\\\\\\\\\\\\\\\\
+
+// int main()
+// {
+//     int k;
+//     for (k = -3; k < -5; k++)
+//         printf("Hello ");
+//     return 0;
+// }
+
+
+
+// int main() {
+//     int i = 0;
+//     while (i < 3) {
+//         i++;
+//     }
+// printf("%d",i);
+// return 0;
+// }
+
+
+
+// int main() {
+//     int a = 0;
+
+//     for (int i = 0; i < 5; i++) {
+//         a++;
+//         continue;
+//     }
+
+//     printf("%d %d", i, a);
+
+//     return 0;
+// }
+
+
+
+int main() {
+    int a = 0, i;
+
+    for (i = 0; i < 5; i++) {
+        continue;
+        a++;
+    }
+
+    printf("%d %d", i, a);
+    return 0;
+}
+
+// int main() {
+//     int i = 0;
+//     while (i = 0) {
+//         printf("Hello\n");
+//     }
+//     return 0;
+// }
