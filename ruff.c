43b32b84ba04d16f43582f4b8028acc0600883ee
mahbubur3 <mahbubur9@yahoo.com>
2024-05-14 14:54:28 +0600

    updated

--- ruff.c
+++ ruff.c
@@ -702,7 +702,7 @@ int main() {
 
 
 //* matrix
-int main() {
+/* int main() {
     int n;
     scanf("%d", &n);
  
@@ -739,4 +739,37 @@ int main() {
     printf("%d", convertAbsolute);
     
     return 0;
-}  
+}   */
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    int a[n][m];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    int x;
+    scanf("%d", &x);
+
+    int flag = 0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (a[i][j] == x) {
+                flag = 1;
+            }
+        }
+    }
+
+    if (flag == 0) {
+        printf("will take number\n");
+    } else {
+        printf("will not take number\n");
+    }     
+
+    return 0;
+}
