60e428f3fd0f2f542411135920f92e73a94ce93c
mahbubur3 <mahbubur9@yahoo.com>
2024-05-16 14:44:01 +0600

    updated

--- ruff.c
+++ ruff.c
@@ -835,3 +835,59 @@ int main() {
     return 0;
 } */
 
+/* int main() {
+    int n, k;
+    scanf("%d %d", &n, &k);
+
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (a[i] < a[j]) {
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+
+    int sum = 0;
+    for (int i = 0; i < k; i++) {
+        sum += a[i];
+    }
+
+    printf("%d", sum);
+
+    return 0;
+} */
+
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    int min = a[0], count = 0;
+    for (int i = 0; i < n; i++) {
+        if (a[i] < min) {
+            min = a[i];
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (a[i] == min) {
+            count++;
+        }
+    }
+
+    printf("%d", count);
+
+    return 0;
+} 
