74dc62b059ece381b429830540556d37adc5ea27
mahbubur3 <mahbubur9@yahoo.com>
2024-05-18 10:06:39 +0600

    updated

--- AssiutUniversityProblems.c
+++ AssiutUniversityProblems.c
@@ -1966,3 +1966,35 @@ int main() {
 
     return 0;
 } */
+
+
+/* int main() {
+    int n;
+    scanf("%d", &n);
+
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    int i = 0, j = n - 1, isPalindrome = 1;
+
+    while (i < j) {
+        if (a[i] != a[j]) {
+            isPalindrome = 0;
+            break;
+        } else {
+            i++;
+            j--;
+        }
+    }
+
+    if (isPalindrome == 1) {
+        printf("YES\n");
+    } else {
+        printf("NO\n");
+    }
+
+    return 0;
+} */
+
