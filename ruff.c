3b0c361b1d5d1cfdae0bd1cbef377e44a8612a18
Mahbubur Rahman <mahbubur9@yahoo.com>
2024-05-23 20:38:54 +0600

    Update ruff.c

--- ruff.c
+++ ruff.c
@@ -1033,3 +1033,910 @@ int main() {
 
     return 0;
 } */
+
+
+
+//---//----//----///-----/////---///---///--//
+/*
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        printf("Hello\n");
+    }
+    
+    return 0;
+}
+*/
+
+
+// int main() {
+//     for (int i = 1; i <= 10; i++) {
+//         printf("%d\n", i);
+//     }
+    
+//     return 0;
+// }
+
+
+
+// int main() {
+//     for (int i = 2; i <= 10; i = i + 2) {
+//         printf("%d\n", i);
+//     }
+    
+//     return 0;
+// }
+
+
+// int main() {
+//     for (int i = 20; i >= 1; i--) {
+//         printf("%d\n", i);
+//     }
+    
+//     return 0;
+// }
+
+
+/*
+int main() {
+    int sum = 0, n;
+    scanf("%d", &n);
+    
+    for (int i = 1; i <= n; i++) {
+        sum = sum + i;
+    }
+    
+    printf("%d", sum);
+    
+    return 0;
+}
+*/
+
+
+/*
+int main() {
+    int sum = 0;
+    
+    for(int i = 1; i <= 100; i++) {
+        sum = sum + i;
+    }
+    
+    printf("%d", sum);
+    
+    return 0;
+}
+*/
+
+
+/*
+int main() {
+    for (int i = 1; i <= 20; i++) {
+        
+        if(i % 2 == 0) {
+            printf("%d - even\n", i);
+        } else {
+            printf("%d - odd\n", i);
+        }
+    }
+    
+    return 0;
+}
+*/
+
+
+/*
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        
+        if(i == 5) {
+            break;
+        }
+        
+        printf("%d", i);
+    }
+    
+    return 0;
+}
+*/
+
+
+/*
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        
+        printf("%d", i);
+        
+        if(i == 5) {
+            break;
+        }
+    }
+    
+    return 0;
+}
+*/
+
+
+/*
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        
+        if(i == 5) {
+            continue;
+        }
+        
+        printf("%d", i);
+    }
+    
+    return 0;
+}
+*/
+
+
+// int main() {
+//     int i = 1;
+    
+//     while (i <= 10) {
+//         printf("%d\n", i);
+//         i++;
+//     }
+    
+//     return 0;
+// }
+
+
+/*
+int main() {
+    int i = 1;
+    
+    while (i <= 10) {
+        if (i == 6) {
+            break;
+        }
+        
+        printf("%d", i);
+        i++;
+    }
+    
+    return 0;
+}
+*/
+
+
+/*
+int main() {
+    int i = 1;
+    
+    do {
+        printf("%d\n", i);
+        i++;
+    }
+    while (i <= 5);
+    
+    return 0;
+}
+*/
+
+
+
+// QUIZ \\\\\\\\\\\\\\\\\\\\\\\\\\
+
+// int main()
+// {
+//     int k;
+//     for (k = -3; k < -5; k++)
+//         printf("Hello ");
+//     return 0;
+// }
+
+
+
+// int main() {
+//     int i = 0;
+//     while (i < 3) {
+//         i++;
+//     }
+// printf("%d",i);
+// return 0;
+// }
+
+
+
+// int main() {
+//     int a = 0;
+    
+//     for (int i = 0; i < 5; i++) {
+//         a++;
+//         continue;
+//     }
+    
+//     printf("%d %d", i, a);
+    
+//     return 0;
+// }
+
+
+
+// int main() {
+//     int a = 0, i;
+    
+//     for (i = 0; i < 5; i++) {
+//         continue;
+//         a++;
+//     }
+    
+//     printf("%d %d", i, a);
+//     return 0;
+// }
+
+
+// int main() {
+//     int i = 0;
+//     while (i = 0) {
+//         printf("Hello\n");
+//     }
+//     return 0;
+// }
+
+
+/*
+int main() {
+    long long int x, y;
+    scanf("%lld %lld", &x, &y);
+    
+    long long int sum = x + y;
+    long long int mul = x * y;
+    long long int sub = x - y;
+    
+    printf("%lld + %lld = %lld\n", x, y, sum);
+    printf("%lld * %lld = %lld\n", x, y, mul);
+    printf("%lld - %lld = %lld\n", x, y, sub);
+    
+    return 0;
+}
+*/
+
+// int main() {
+//     long long int a, b, c, d;
+//     scanf("%lld %lld %lld %lld", &a, &b, &c, &d);
+    
+//     long long int equation = (a * b) - (c * d);
+//     printf("Difference = %lld", equation);
+    
+//     return 0;
+// }
+
+
+/*
+int main() {
+    int num;
+    scanf("%d", &num);
+    
+    int nums;
+    int even = 0, odd = 0, pos = 0, neg = 0;
+    for(int i = 1; i <= num; i++) {
+        scanf("%d", &nums);
+        
+        if (nums % 2 == 0) {
+            even++;
+        } else {
+            odd++;
+        }
+        
+        if (nums > 0) {
+            pos++;
+        } else {
+            neg++;
+        }
+    }
+    
+    printf("%d", even);
+    
+    return 0;
+}
+*/
+
+// #include <limits.h>
+// max = INT_MIN, min = INT_MAX;
+
+
+// int main() {
+//     int n;
+//     scanf("%d", &n);
+    
+//     for (int i = 1; i <= n; i++) {
+//         if (n % i == 0) {
+//             printf("%d\n", i);
+//         }
+//     }
+    
+//     return 0;
+// }
+
+
+// int main() {
+//     int a, b;
+//     scanf("%d %d", &a, &b);
+    
+//     int sub = a - b;
+//     if (sub >= 0) {
+//         printf("%d", sub);
+//     } else {
+//         printf("%d", 0);
+//     }
+    
+//     return 0;
+// }
+
+
+// int main() {
+//     int n;
+//     scanf("%d", &n);
+    
+//     int lastDigit = n % 10;
+//     int firstDigit = n/10;
+//     if (lastDigit % firstDigit == 0 || firstDigit % lastDigit == 0) {
+//         printf("YES\n");
+//     } else {
+//         printf("YES\n");
+//     }
+    
+//     return 0;
+// }
+
+
+// int main() {
+//     char a;
+//     scanf("%c", &a);
+    
+//     if (a == 'z') {
+//         printf("a");
+//     } else {
+//         char result = a + 1;
+//         printf("%c", result);
+//     }
+    
+//     return 0;
+// }
+
+/*
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    
+    if (a + b - c == d) {
+        printf("YES\n");
+    } else if (a - b + c == d) {
+        printf("YES\n");
+    } else if (a + b * c == d) {
+        printf("YES\n");
+    } else if (a - b * c == d) {
+        printf("YES\n");
+    } else if (a * b + c == d) {
+        printf("YES\n");
+    } else if (a * b - c == d) {
+        printf("YES\n");
+    } else {
+        printf("NO\n");
+    }
+     
+    return 0;
+}
+*/
+
+
+/*
+int main() {
+    int nums[5];
+    
+    for (int i = 0; i < 5; i++) {
+        scanf("%d", &nums[i]);
+    }
+    
+    for (int i = 0; i < 5; i++) {
+        printf("%d\n", nums[i]);
+    }
+    
+    return 0;
+}
+*/
+
+/*
+int main() {
+    int ar[3];
+    
+    for (int i = 0; i < 3; i++) {
+        scanf("%d", &ar[i]);
+    }
+    
+    for (int i = 0; i < 3; i++) {
+        printf("%d\n", ar[i]);
+    }
+    
+    return 0;
+}
+*/
+
+/*
+// array initializing
+int main() {
+    int nums[5] = {10, 20, 30, 40, 50};
+    
+    for (int i = 0; i < 5; i++) {
+        printf("%d\n", nums[i]);
+    }
+    
+    return 0;
+}
+*/
+
+
+// if i need zero
+// int main() {
+//     int nums[5] = {0};
+    
+//     for (int i = 0; i < 5; i++) {
+//         printf("%d\n", nums[i]);
+//     }
+    
+//     return 0;
+// }
+
+
+/*
+int main() {
+    int n;
+    scanf("%d", &n);
+    int nums[n];
+    
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+    }
+    
+    for (int i = n-1; i >= 0; i--) {
+        printf("%d ", nums[i]);
+    }
+    
+    return 0;
+}
+*/
+
+
+/*
+int main() {
+    int n;
+    scanf("%d", &n);
+    int nums[n];
+    
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+    }
+    
+    for (int i = 0; i < n; i++) {
+        if (nums[i] % 2 == 0) {
+            printf("%d\n", nums[i]);
+        }
+    }
+    
+    return 0;
+}
+*/
+
+
+/*
+int main() {
+    int n;
+    scanf("%d", &n);
+    int nums[n];
+    
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+    }
+    
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum = sum + nums[i];
+    }
+    
+    printf("%d", sum);
+    
+    return 0;
+}
+*/
+
+/*
+int main() {
+    int n;
+    scanf("%d", &n);
+    int nums[n];
+    
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+    }
+    
+    for (int i = n-1; i > 0; i--) {
+        if (i % 2 != 0) {
+            printf("%d ", nums[i]);
+        }
+    }
+    
+    return 0;
+}
+*/
+
+
+
+/*
+#include <stdio.h>
+
+int main()
+{
+    long long eye, mouth, body, num;
+    scanf("%lld %lld %lld", &eye, &mouth, &body);
+    num = 0;
+
+    if (eye != 0 && body != 0) {
+        if (mouth >= eye && mouth >= body && eye != 0 && body != 0) {
+            if (eye >= body) {
+                num = bodyf;
+            } else if (body >= eye) {
+                num = eye;
+            }
+            
+        } else if (eye <= mouth && eye <= body) {
+            num = eye;
+        } else if (body <= mouth && body <= eye) {
+            num = body;
+        } else if (mouth <= body && mouth <= eye) {
+            num += mouth;
+            eye -= mouth;
+            body -= mouth;
+
+            if (eye / 2 <= body) {
+                if (eye % 2 == 0) {
+                    eye = eye / 2;
+                    num += eye;
+                } else {
+                    eye -= 1;
+                    eye = eye / 2;
+                    num += eye;
+                }
+            } else {
+                num += body;
+            }
+        }
+    }
+    
+    printf("%lld", num);
+}
+*/
+
+
+/*
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+    
+    for(int i = n-1; i >= 0; i--){
+        printf("%d", a[i]);
+    }
+    
+    return 0;
+}
+*/
+
+
+/*
+// insert element from an array 
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    int a[n+1];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+    
+    int p, v;
+    scanf("%d %d", &p, &v);
+    for (int i = n; i >= p+1; i--) {
+        a[i] = a[i-1];
+    }
+    a[p] = v;
+    
+    for (int i = 0; i <= n; i++) {
+        printf("%d ", a[i]);
+    }
+    
+    return 0;
+}
+*/
+
+
+/*
+// delete element from an array
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+    
+    int p;
+    scanf("%d", &p);
+    
+    for (int i = p; i < n-1; i++) {
+        a[i] = a[i + 1];
+    }
+    
+    for (int i = 0; i < n-1; i++) {
+        printf("%d ", a[i]);
+    }
+    
+    return 0;
+}
+*/
+
+
+
+// reverse array elements
+// int main() {
+//     int n;
+//     scanf("%d", &n);
+    
+//     int a[n];
+//     for (int i = 0; i < n; i++) {
+//         scanf("%d", &a[i]);
+//     }
+    
+//     int i = 0, j = n-1;
+//     while (i < j) {
+//         int temp; 
+//         temp = a[i];
+//         a[i] = a[j];
+//         a[j] = temp;
+//         i++;
+//         j--;
+//     }
+    
+//     for (int i = 0; i < n; i++) {
+//         printf("%d ", a[i]);
+//     }
+    
+//     return 0;
+// }
+
+
+
+/*
+// add two array together
+int main() {
+    int n1;
+    scanf("%d", &n1);
+    
+    int a1[n1];
+    for (int i = 0; i < n1; i++) {
+        scanf("%d", &a1[i]);
+    }
+    
+    int n2;
+    scanf("%d", &n2);
+    
+    int a2[n2];
+    for (int i = 0; i < n2; i++) {
+        scanf("%d", &a2[i]);
+    }
+    
+    int fullArray[n1+n2];
+    for (int i = 0; i < n1; i++) {
+        fullArray[i] = a1[i];
+    }
+    
+    int i = n1;
+    for (int j = 0; j < n2; j++) {
+        fullArray[i] = a2[j];
+        i++;
+    }
+    
+    for (int i = 0; i < n1+n2; i++) {
+        printf("%d ", fullArray[i]);
+    }
+        
+    return 0;   
+}
+*/
+
+
+// WORKING WITH STRING ===================================================
+
+// int main() {
+//     char a[6] = "Mahbub";
+    
+//     for (int i = 0; i < 6; i++) {
+//         printf("%c\n", a[i]);
+//     }
+    
+//     return 0;
+// }
+
+
+// int main() {
+//     char a[7] = "Mahbub";
+//     printf("%s", a);
+    
+//     return 0;
+// }
+
+
+
+// string input
+// int main() {
+//     char name[7];
+//     scanf("%s", name);
+//     printf("%s\n", name);
+    
+//     return 0;
+// }
+
+
+// gets function error on online & fgets is okay.
+// #include <string.h>
+// int main() {
+//     char a[100];
+//     fgets(a, 16, stdin);
+//     printf("%s", a);
+    
+//     return 0;
+// }
+
+
+// print 10 letter using null 
+// #include <string.h>
+// int main() {
+//     char a[100];
+//     fgets(a, 16, stdin);
+//     a[10] = '\0';
+//     printf("%s", a);
+    
+//     return 0;
+// }
+
+
+// #include <string.h>
+// int main() {
+//     char name[20];
+//     fgets(name, 14, stdin);
+//     printf("%s", name);
+    
+//     return 0;
+// }
+
+
+// check array length
+/*
+int main() {
+    char a[100];
+    scanf("%s", a);
+    
+    int count = 0, i = 0;
+    while (a[i] != '\0') {
+        i++;
+        count++;
+    }
+    
+    printf("%d", count);
+    
+    return 0;
+}
+*/
+
+// with function
+// #include <string.h>
+// int main() {
+//     char a[100];
+//     scanf("%s", a);
+    
+//     int sizeOfString = strlen(a);
+//     printf("%d\n", sizeOfString);
+    
+//     return 0;
+// }
+
+
+/*
+void changeIt(int* arr, int n) {
+    arr[n-1] = 100;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    changeIt(arr, n);
+    
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
+*/
+
+
+// void printNumbers(void) {
+//     int x;
+//     scanf("%d", &x);
+
+//     for (int i = 1; i <= x; i++) {
+//         printf("%d ", i);
+//     }
+// }
+
+// int main() {
+//     printNumbers();
+
+//     return 0;
+// }
+
+// void swapNum(int* x, int* y) {
+//     int temp = *x;
+//     *x = *y;
+//     *y = temp;
+// }
+
+// int main() {
+//     int x, y;
+//     scanf("%d %d", x, y);
+//     // int x = 20, y = 10;
+
+//     swapNum(&x, &y);
+//     printf("%d %d", x, y);
+
+//     return 0;
+// }
+
+
+int main() {
+    int n, s, d, h;
+    scanf("%d", &n);
+
+    s = n - 1;
+    h = 1;
+
+    for (int i = 1; i <= (2*n)- 1; i++) {
+        for (int j = 1; j <= s; j++) {
+            printf(" ");
+        }
+
+        for (int j = 1; j <= h; j++) {
+            if (i % 2 == 0) {
+                printf("-");
+            } else {
+                printf("#");
+            }
+            
+        }
+
+        if (i < n) {
+            s--;
+            h += 2;
+        } else {
+            s++;
+            h -= 2;
+        }
+        
+        printf("\n");
+    }
+
+    return 0;
+}
